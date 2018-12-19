72aa81a7e633847e37708d610fbee709da9fac89
Danijel Sugar <danijel.sugar@outlook.com>
2018-12-19 18:59:05 +0100

    Update products done

--- update_product.php
+++ update_product.php
@@ -26,7 +26,23 @@
 	$pageTitle = 'Update Product';
 	include_once 'layout_header.php';
 
+	//if the form have been submited
+	if(isset($_POST["edit"])){
+
+		//set product property values
+		$product->fullname = $_POST["fullname"];
+		$product->price = $_POST["price"];
+		$product->description = $_POST["description"];
+		$product->category_id = $_POST["category_id"];
+
+		if($product->update()){
+			echo '<div class="alert alert-success">Product has been updated.</div>';
+		}else{
+			echo '<div class="alert alert-danger">Unable to update product.</div>';
+		}
+	}
 ?>
+
 
 	<div class="row">
         <div class="col 12">
@@ -34,6 +50,50 @@
     			<a href="index.php" class="btn btn-primary pull-right">Read products</a>
     		</div>
         </div>
+	</div>
+
+	<div class="row">
+		<div class="col 12">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id=$id");?>" method="post">
+				<div class="form-group">
+					<label for="fullname">Name</label>
+					<input type="text" name="fullname" class="form-control" value="<?php echo $product->fullname; ?>">
+				</div>
+				<div class="form-group">
+					<label for="price">Price</label>
+					<input type="number" name="price" class="form-control" value="<?php echo $product->price; ?>">
+				</div>
+				<div class="form-group">
+					<label for="description">Description</label>
+					<input type="text" name="description" class="form-control" value="<?php echo $product->description; ?>">
+				</div>
+				<div class="form-group">
+					<label for="category_id">Category</label>
+					<select class="custom-select" name="category_id">
+						<option value="0">Select category...</option>
+						<?php 
+
+							$stmt = $category->read();
+
+							while($rowCategory = $stmt->fetch(PDO::FETCH_ASSOC)){
+								$categoryId = $rowCategory['id'];
+								$categoryFullname = $rowCategory['fullname'];
+
+								if($product->category_id==$categoryId){
+									echo "<option value='$categoryId' selected>";
+								}else{
+									echo "<option value='$categoryId'>";
+								}
+
+								echo "$categoryFullname</option>";
+							}
+						
+						?>
+					</select>
+				</div>
+				<input class="btn btn-primary" type="submit" name="edit" value="Edit">
+			</form>
+		</div>
 	</div>
 
 <?php 
